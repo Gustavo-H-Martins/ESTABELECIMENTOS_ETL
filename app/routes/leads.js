62646a2189c3923e3d335692654b5b434951963d
Gustavo Henrique Lopes Martins <70769813+Gustavo-H-Martins@users.noreply.github.com>
2023-04-17 09:20:53 -0300

    recurso counts
    Signed-off-by: Gustavo Henrique Lopes Martins <70769813+Gustavo-H-Martins@users.noreply.github.com>

--- app/routes/leads.js
+++ app/routes/leads.js
@@ -3,7 +3,7 @@
  * Libs
  */
 const db = require('../config/db');
-const logToDatabase = require ('../config/log')
+const logToDatabase = require('../config/log')
 const router = require('express').Router();
 /**
  * Conexão com o DB
@@ -15,41 +15,41 @@ const router = require('express').Router();
 
 // GetAll
 router.route("/estabelecimentos")
-    .get(function(req, res, next){
-        const clientIp = req.ip;
-        const bandeira = req.query.bandeira ? [req.query.bandeira.toUpperCase()] : `TICKET`;
-        const uf = req.query.uf ? [req.query.uf.toUpperCase()] : null;
-        const cidade = req.query.cidade ? [req.query.cidade.toUpperCase().replace(/-/g, ' ')] : null;
-        const bairro = req.query.bairro ? [req.query.bairro.toUpperCase().replace(/-/g, ' ')] : null;
-        const page = parseInt(req.query.page) || 1;
-        const pageSize = parseInt(req.query.pageSize) || 100;
-        const offset = (page - 1) * pageSize;
+  .get(function(req, res, next) {
+    const clientIp = req.ip;
+    const bandeira = req.query.bandeira ? [req.query.bandeira.toUpperCase()] : `TICKET`;
+    const uf = req.query.uf ? [req.query.uf.toUpperCase()] : null;
+    const cidade = req.query.cidade ? [req.query.cidade.toUpperCase().replace(/-/g, ' ')] : null;
+    const bairro = req.query.bairro ? [req.query.bairro.toUpperCase().replace(/-/g, ' ')] : null;
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.pageSize) || 100;
+    const offset = (page - 1) * pageSize;
 
-        let query = `SELECT * FROM tb_${bandeira.toLocaleLowerCase()}`;
-        let conditions = [];
-        if (bandeira) conditions.push(`BANDEIRA = "${bandeira}"`);
-        if (uf) conditions.push(`UF = "${uf}"`);
-        if (cidade) conditions.push(`CIDADE = "${cidade}"`);
-        if (bairro) conditions.push(`BAIRRO = "${bairro}"`);
-        if (conditions.length > 0) query += ` WHERE ${conditions.join(' AND ')}`;
-        query += ` LIMIT ? OFFSET ?`;
-        
-        db.all(query, [pageSize, offset], (err, rows) => {
-            if (err){
-                res.status(500).json({error: err.message});
-                return;
-            }   
-                // Formatando data e hora para incluir no log
-                const date = new Date()
-                const formattedDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`; // formata a data como DD/MM/AAAA
-                const formattedTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`; // formata a hora como HH:MM:SS
-                dataChamada = `Data: ${formattedDate} - Hora: ${formattedTime}`
-                logToDatabase(clientIp,`Retornando ${rows.length} dados de "${bandeira}" no estado de "${uf}" cidade de ${cidade}`, 'INFO', dataChamada)
-                res.status(200).json(
-                    rows
-                );
-        });
+    let query = `SELECT * FROM tb_${bandeira.toLocaleLowerCase()}`;
+    let conditions = [];
+    if (bandeira) conditions.push(`BANDEIRA = "${bandeira}"`);
+    if (uf) conditions.push(`UF = "${uf}"`);
+    if (cidade) conditions.push(`CIDADE = "${cidade}"`);
+    if (bairro) conditions.push(`BAIRRO = "${bairro}"`);
+    if (conditions.length > 0) query += ` WHERE ${conditions.join(' AND ')}`;
+    query += ` LIMIT ? OFFSET ?`;
+
+    db.all(query, [pageSize, offset], (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      // Formatando data e hora para incluir no log
+      const date = new Date()
+      const formattedDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`; // formata a data como DD/MM/AAAA
+      const formattedTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`; // formata a hora como HH:MM:SS
+      dataChamada = `Data: ${formattedDate} - Hora: ${formattedTime}`
+      logToDatabase(clientIp, `Retornando ${rows.length} dados de "${bandeira}" no estado de "${uf}" cidade de ${cidade}`, 'INFO', dataChamada)
+      res.status(200).json(
+        rows
+      );
     });
+  })
 // GetAll-contagem
 router.route("/estabelecimentos/counts")
   .get(function(req, res, next) {
@@ -57,13 +57,15 @@ router.route("/estabelecimentos/counts")
     const bandeira = req.query.bandeira ? [req.query.bandeira.toUpperCase()] : `TICKET`;
     const uf = req.query.uf ? [req.query.uf.toUpperCase()] : null;
     const cidade = req.query.cidade ? [req.query.cidade.toUpperCase().replace(/-/g, ' ')] : null;
+    const bairro = req.query.bairro ? [req.query.bairro.toUpperCase().replace(/-/g, ' ')] : null;
     const groupby = req.query.groupby ? [req.query.groupby.toLocaleUpperCase()] : null;
 
-    let query = `SELECT COUNT(*) as TOTAL FROM tb_${req.query.bandeira.toLowerCase()}`;
+    let query = `SELECT COUNT(*) as TOTAL FROM tb_${bandeira.toLocaleLowerCase()}`;
     let conditions = [];
     if (bandeira) conditions.push(`BANDEIRA = "${bandeira}"`);
-    if (uf) conditions.push(`Estado = "${uf}"`);
-    if (cidade) conditions.push(`Municipio = "${cidade}"`);
+    if (uf) conditions.push(`UF = "${uf}"`);
+    if (cidade) conditions.push(`CIDADE = "${cidade}"`);
+    if (bairro) conditions.push(`BAIRRO = "${bairro}"`);
     if (conditions.length > 0) query += ` WHERE ${conditions.join(' AND ')}`;
     if (groupby) query += ` GROUP BY ${groupby}`;
     if (groupby) query = query.replace('COUNT(*)', `${groupby}, COUNT(*)`)
@@ -81,7 +83,7 @@ router.route("/estabelecimentos/counts")
       const formattedDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`; // formata a data como DD/MM/AAAA
       const formattedTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`; // formata a hora como HH:MM:SS
       dataChamada = `Data: ${formattedDate} - Hora: ${formattedTime}`
-      logToDatabase(clientIp, `Retornando ${rows.length} dados de "${bandeira}" no estado de "${uf}" município de ${cidade}`, 'INFO', dataChamada)
+      logToDatabase(clientIp, `Retornando ${rows.length} dados de "${bandeira}" no estado de "${uf}" cidade de ${cidade}`, 'INFO', dataChamada)
       res.status(200).json(
         rows
       );
