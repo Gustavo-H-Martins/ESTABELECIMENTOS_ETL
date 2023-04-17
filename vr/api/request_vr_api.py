704cb42914b99d2cc873bbbbfaea21509730dc84
Gustavo Henrique Lopes Martins <70769813+Gustavo-H-Martins@users.noreply.github.com>
2023-04-17 11:27:27 -0300

    inclusão da etapa VR
    Signed-off-by: Gustavo Henrique Lopes Martins <70769813+Gustavo-H-Martins@users.noreply.github.com>

---
+++ vr/api/request_vr_api.py
@@ -0,0 +1,33 @@
+def get_vr(latitude:str='-19.919052', longitude:str='-43.9386685', raio:str='10' ,distancia:str='10'):
+    import requests
+    import json
+    url = "https://mapaec.vrbeneficios.io/search/buscaec?"
+
+    payload={}
+    # parâmetros da consulta    
+    params = {
+        'lat': latitude,
+        'lon': longitude,
+        'raio': raio,
+        'distancia': distancia,
+        'produto': '00031',
+        'termo':''
+        }
+    headers = {
+    'accept': 'application/json, text/plain, */*',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+    'origin': 'https://portal.vr.com.br',
+    'referer': 'https://portal.vr.com.br/',
+    'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'cross-site',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    base = response.json()
+    print(base)
